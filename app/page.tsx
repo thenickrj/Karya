e5b10bd7b2b0d3586ed0c16c53a77aefa6c51ed9
Nikhil <nikhilbangalor@gmail.com>
2026-07-22 21:03:13 +0530

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Karya</h1>
-      <h4>What Matters Now</h4>
+    <div className="relative min-h-screen">
+      <Link
+        className="login-button absolute right-4 top-4 text-red-900 p-2"
+        href="/login"
+      >
+        Login
+      </Link>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="mb-4 text-4xl font-bold tracking-tight text-heading md:text-5xl lg:text-6xl">
+          Karya
+        </h1>
+        <h4>What Matters Now</h4>
+        <a
+          href="/todo"
+          className="inline-flex items-center text-white bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-base px-5 py-3 focus:outline-none"
+        >
+          Learn more
+        </a>
+      </div>
     </div>
   );
 }
---
+++ pages/login/index.jsx
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import "./style.css";
+
+export default function LoginPage() {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <div className="container" id="container">
+      <div className={`container-inner ${isActive ? "active" : ""}`}>
+        <h1>Login Page</h1>
+        {/* Sign Up Form */}
+        <div className="form-container sign-up">
+          <form onSubmit={(e) => e.preventDefault()}>
+            <h1>Create Account</h1>
+            <div className="social-icons">
+              <a href="#" className="icon">
+                <i className="fa-brands fa-google-plus-g"></i>
+              </a>
+              <a href="#" className="icon">
+                <i className="fa-brands fa-facebook-f"></i>
+              </a>
+              <a href="#" className="icon">
+                <i className="fa-brands fa-github"></i>
+              </a>
+              <a href="#" className="icon">
+                <i className="fa-brands fa-linkedin-in"></i>
+              </a>
+            </div>
+            <span>or use your email for registration</span>
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button type="submit">Sign Up</button>
+          </form>
+        </div>
+
+        {/* Sign In Form */}
+        <div className="form-container sign-in">
+          <form onSubmit={(e) => e.preventDefault()}>
+            <h1>Sign In</h1>
+            <div className="social-icons">
+              <a href="#" className="icon">
+                <i className="fa-brands fa-google-plus-g"></i>
+              </a>
+              <a href="#" className="icon">
+                <i className="fa-brands fa-facebook-f"></i>
+              </a>
+              <a href="#" className="icon">
+                <i className="fa-brands fa-github"></i>
+              </a>
+              <a href="#" className="icon">
+                <i className="fa-brands fa-linkedin-in"></i>
+              </a>
+            </div>
+            <span>or use your email password</span>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <a href="#">Forget Your Password?</a>
+            <button type="submit">Sign In</button>
+          </form>
+        </div>
+
+        {/* Toggle overlay */}
+        <div className="toggle-container">
+          <div className="toggle">
+            <div className="toggle-panel toggle-left">
+              <h1>Welcome Back!</h1>
+              <p>Enter your personal details to use all of site features</p>
+              <button
+                className="hidden"
+                onClick={() => setIsActive(false)}
+                type="button"
+              >
+                Sign In
+              </button>
+            </div>
+            <div className="toggle-panel toggle-right">
+              <h1>Hello, Friend!</h1>
+              <p>
+                Register with your personal details to use all of site features
+              </p>
+              <button
+                className="hidden"
+                onClick={() => setIsActive(true)}
+                type="button"
+              >
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
